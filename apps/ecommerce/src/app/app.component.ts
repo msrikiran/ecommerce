5db0cec6ae8c050d142e4b46dd1047f4436d6516
msrikiran <msrikiran@gmail.com>
2024-12-09 09:22:55 +0530

    hm prod header footer creaed

--- apps/ecommerce/src/app/app.component.ts
+++ apps/ecommerce/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FooterComponent, HeaderComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
